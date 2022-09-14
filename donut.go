ec2d6fd670dbcef927d80785218b10a45f3ca62d
quincinia <eanj13@gmail.com>
2022-09-14 09:47:13 -0700

    Using frame package

--- donut.go
+++ donut.go
@@ -3,101 +3,14 @@ package main
 import (
 	"fmt"
 	"log"
-	"math"
 	"os"
 	"time"
 
+	"github.com/quincinia/donut/frame"
 	"golang.org/x/term"
 )
 
-const (
-	thetaSpacing  = 0.07
-	phiSpacing    = 0.02
-	frameDelay    = 50 // milliseconds
-)
-
-const (
-	R1 = 1.0
-	R2 = 2.0
-	K2 = 5.0
-)
-
-// Experimenting with generics.
-func newMatrix[T any](dim int) *[][]T {
-	matrix := make([][]T, dim)
-	for i := range matrix {
-		matrix[i] = make([]T, dim)
-	}
-	return &matrix
-}
-
-var (
-	newFrame   = newMatrix[rune]
-	newZBuffer = newMatrix[float64]
-)
-
-// renderFrame writes a frame of the animation to stdout.
-// A and B are the rotation angles (in radians) around the X and Z axes, respectively.
-func renderFrame(A, B float64, dim int) {
-	cosA, sinA := math.Cos(A), math.Sin(A)
-	cosB, sinB := math.Cos(B), math.Sin(B)
-
-	frame, zbuffer := newFrame(dim), newZBuffer(dim)
-	for i := range *frame {
-		for j := range (*frame)[i] {
-			(*frame)[i][j] = ' '
-		}
-	}
-
-	K1 := float64(dim) * K2 * 3 / (8 * (R1 + R2))
-
-	// theta goes around the cross-sectional circle of a torus
-	for theta := 0.0; theta < 2.0*math.Pi; theta += thetaSpacing {
-		cosTheta, sinTheta := math.Cos(theta), math.Sin(theta)
-
-		// phi goes around the center of revolution of a torus
-		for phi := 0.0; phi < 2.0*math.Pi; phi += phiSpacing {
-			cosPhi, sinPhi := math.Cos(phi), math.Sin(phi)
-
-			// the x,y coordinate of the circle, before revolving (factored out of the above equations)
-			circleX := R2 + R1*cosTheta
-			circleY := R1 * sinTheta
-
-			// final 3D (x,y,z) coordinate after rotations, directly from our math above
-			x := circleX*(cosB*cosPhi+sinA*sinB*sinPhi) - circleY*cosA*sinB
-			y := circleX*(sinB*cosPhi-sinA*cosB*sinPhi) + circleY*cosA*cosB
-			z := K2 + cosA*circleX*sinPhi + circleY*sinA
-			ooz := 1 / z // "one over z"
-
-			// x and y projection.  note that y is negated here, because y
-			// goes up in 3D space but down on 2D displays.
-			xp := int(float64(dim)/2 + K1*ooz*x)
-			yp := int(float64(dim)/2 - K1*ooz*y)
-
-			// calculate luminance.  ugly, but correct.
-			L := cosPhi*cosTheta*sinB - cosA*cosTheta*sinPhi - sinA*sinTheta + cosB*(cosA*sinTheta-cosTheta*sinA*sinPhi)
-			// L ranges from -sqrt(2) to +sqrt(2).  If it's < 0, the surface
-			// is pointing away from us, so we won't bother trying to plot it.
-			if L > 0 {
-				// test against the z-buffer.  larger 1/z means the pixel is
-				// closer to the viewer than what's already plotted.
-				if ooz > (*zbuffer)[xp][yp] {
-					(*zbuffer)[xp][yp] = ooz
-					luminanceIndex := int(L * 8)
-					// luminanceIndex is now in the range 0..11 (8*sqrt(2) = 11.3)
-					// now we lookup the character corresponding to the
-					// luminance and plot it in our output:
-					(*frame)[xp][yp] = []rune(".,-~:;=!*#$@")[luminanceIndex]
-				}
-			}
-		}
-	}
-
-	for i := range *frame {
-		fmt.Println(string((*frame)[i]))
-	}
-	fmt.Printf("\033[%dF", dim)
-}
+const frameDelay = 50 // milliseconds
 
 func main() {
 	width, height, err := term.GetSize(int(os.Stdin.Fd()))
@@ -111,19 +24,49 @@ func main() {
 	} else {
 		// If we're constrained by height, we subtract 1 because fmt.Println adds an extra line.
 		// If we don't do this, we get artifacts when multiple renders occur.
-		min = height-1
+		min = height - 1
 	}
 
+	ascii := frame.NewASCII(min, min, nil)
+	// The orginal SetFunc as described in the original implementation.
+	// Assumes L ranges from -sqrt(2) to +sqrt(2).
+	ascii.SF = frame.SetFunc[rune](func(L float64) (rune, bool) {
+		// L ranges from -sqrt(2) to +sqrt(2).  If it's < 0, the surface
+		// is pointing away from us, so we won't bother trying to plot it.
+		if L > 0 {
+			luminanceIndex := int(L * 8)
+			// luminanceIndex is now in the range 0..11 (8*sqrt(2) = 11.3)
+			// now we lookup the character corresponding to the
+			// luminance and plot it in our output:
+			return []rune(".,-~:;=!*#$@")[luminanceIndex], true
+		}
+		return 0, false
+	})
+	torus := frame.DefaultTorus()
+	scene := frame.Scene{
+		Width:  min,
+		Height: min,
+		K2:     5.0,
+		LX:     0.0,
+		LY:     1.0,
+		LZ:     -1.0,
+	}
+	scene.CalculateK1(torus.R1, torus.R2)
+
 	// The number of frames to render in a full cycle.
-	steps := 100
+	steps := 200
 
 	// How much the angles should change between each step.
 	startA, startB := 15.0, 25.0
 	stepA, stepB := 0.07, 0.03
 
 	for {
-		for s, A, B := 0, startA, startB; s < steps; s, A, B = s+1, A+stepA, B+stepB {
-			renderFrame(A, B, min)
+		var s int
+		for s, torus.A, torus.B = 0, startA, startB; s < steps; s, torus.A, torus.B = s+1, torus.A+stepA, torus.B+stepB {
+			ascii.SetAll(' ') // Should the Render function clear the frame instead of doing it here?
+			frame.Render(ascii, torus, scene)
+			ascii.Print(os.Stdout)
+			fmt.Printf("\033[%dF", min)
 			time.Sleep(frameDelay * time.Millisecond)
 		}
 	}
